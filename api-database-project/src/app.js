e06f8d7af0f2f464c86e2c983eb08481454c84da
Arthur <gavagearthur@gmail.com>
2025-03-26 16:57:01 +0100

    [API] test pour vps

--- api-database-project/src/app.js
+++ api-database-project/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || '54.36.183.127'; // adresse du serveur
 
 // Middleware to parse JSON requests
 app.use(express.json());
@@ -12,6 +13,6 @@ const routes = require('./routes/index');
 app.use('/api', routes);
 
 // Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, HOST, () => {
+    console.log(`Server is running on http://${HOST}:${PORT}`);
 });
