1994017d960e861716a51c18987a2a7ab5dcdc5f
Arthur <gavagearthur@gmail.com>
2025-03-26 17:40:58 +0100

    [API]suite

---
+++ test-db.js
@@ -0,0 +1,13 @@
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+
+const DATABASE_PATH = path.resolve(__dirname, './db/casino.db');
+
+const db = new sqlite3.Database(DATABASE_PATH, sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err.message);
+    } else {
+        console.log('Connected to the SQLite database.');
+    }
+    db.close();
+});
