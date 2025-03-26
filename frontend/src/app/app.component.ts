dd9b8032f7fcbf8b93a84a600f3ea0960430c60d
Antoine Brazier <antoinefrb.brazier@gmail.com>
2025-03-26 18:59:21 +0100

    page inscription en review

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavigationBarComponent } from './component/navigation-bar/navigation-bar.component';
+import { RegisterComponent } from './component/register/register.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+standalone: true,
+imports:[NavigationBarComponent,RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
