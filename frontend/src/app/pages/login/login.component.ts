676c033c718b5693afef8a17dcfe24981a1d4523
toruke <abenpro@outlook.com>
2025-04-05 12:56:44 +0200

    progression dans la construction de la page login

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { NavigationBarComponent } from '../../component/navigation-bar/navigation-bar.component';
+
 
 @Component({
-  selector: 'app-login',
-  imports: [],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+ 	selector: 'app-login',
+ 	standalone: true,
+ 	imports: [ReactiveFormsModule, MatInputModule, MatButtonModule, NavigationBarComponent],
+ 	templateUrl: './login.component.html',
+ 	styleUrl: './login.component.css'
 })
 export class LoginComponent {
+ 	private formBuilder = inject(FormBuilder);
+	loginFormGroup = this.formBuilder.group({
+ 		'username': ['', [Validators.required]],
+ 		'password': ['', [Validators.required]]
+});
+
+invalidCredentials = false;
+
+login() {
+	}
 
 }
