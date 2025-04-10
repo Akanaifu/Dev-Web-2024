7b2bb65bbebd8f8989fd82942d8f4bc55c4b2ae1
Arthur <gavagearthur@gmail.com>
2025-04-10 12:40:55 +0200

    ajout slider

--- frontend/src/app/pages/stats/stats.component.ts
+++ frontend/src/app/pages/stats/stats.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-stats',
-  imports: [],
+  imports: [MatSliderModule, MatTooltipModule],
   templateUrl: './stats.component.html',
-  styleUrl: './stats.component.css'
+  styleUrl: './stats.component.css',
 })
 export class StatsComponent {
-
 }
