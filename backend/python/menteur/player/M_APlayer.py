45bef82620829a71ada73c01dd8ea46f481745f0
Antoine Brazier <antoinefrb.brazier@gmail.com>
2025-04-16 22:36:42 +0200

    3 fonction pour jouer une pour choisir si on dénonce ou on joue les autres pour effectuer l'action (en cours)

--- backend/python/menteur/player/M_APlayer.py
+++ backend/python/menteur/player/M_APlayer.py
@@ -1,19 +1,34 @@
 from abc import ABC, abstractmethod
 import random
+from backend.python.EnterValue import EnterValue
 from backend.python.menteur.cards.M_CardsSet import M_CardsSet
 from backend.python.shared_games_cards.shared_cards.ACard import ACard
 from backend.python.shared_games_cards.shared_players.APlayer import APlayer
 
 class M_APlayer(APlayer,ABC):
-    def __init__(self, name: str, color: str,bet:int,solde:int,cardsSet:M_CardsSet, life=6):
+    def __init__(self, name: str, color: str,bet:int,solde:int,cardsSet:M_CardsSet):
         super().__init__(name, color, bet,solde,cardsSet)
         self.__name = name
         self.__color = color
         self.__bet = bet
         self.__cardsSet = cardsSet
-        self.__life=life
+        self.__life=6
         self.__score = 0       
+        cardsStack=M_CardsSet
         
+       
+    def my_turn_to_play(self) -> None:
+        enter=EnterValue
+        chose=enter.EnterValue("Dénoncez vous le joueur précédement? (1: Oui, 2: Non) : ", 1, 2)
+        if chose==1 and cardsStack.get_size() > 0:
+            #problème de l'instance de la classe M_CardsSet soit je mets dans le constructeur ou je fais une méthode pour l'initialiser ou je trouve un autre moyen
+            #print les cartes et vérifier si la carte était celle demandé
+            self.play_snitch()
+        else:
+            # Jouer play_card() si le joueur ne dénonce pas
+            self.play_card()
+        
+     
     @abstractmethod
     def play_snitch(self,APlayer) -> None:
         pass
--- backend/python/menteur/player/M_Player.py
+++ backend/python/menteur/player/M_Player.py
@@ -5,6 +5,16 @@ from backend.python.EnterValue import EnterValue
 
 
 class M_Player(M_APlayer):
+    
+    def __init__(self, name: str, color: str,bet:int,solde:int,cardsSet:M_CardsSet) -> None:
+        super().__init__(name, color, bet,solde,cardsSet)
+        self.__name = name
+        self.__color = color
+        self.__bet = bet
+        self.__cardsSet = cardsSet
+        self.__life=6
+        self.__score = 0    
+    
     def play_card(self)->M_CardsSet:
         cards_Chosen = M_CardsSet
         # Je suis dans le jeu console
@@ -13,16 +23,13 @@ class M_Player(M_APlayer):
         for k in range(i-1):
             card_position=enter.EnterValue("Entrez la position de la carte en commençant par la gauche: ", 1, len(self.__cardsSet))
             card_Chose = self.__cardsSet.pop_card(card_position-1)
-            cards_Chosen.append(card_Chose)
+            cards_Chosen.add_card(card_Chose)
         return cards_Chosen
     
     def play_snitch(self,player:M_APlayer,cards_playing:M_CardsSet)->None:
         # Je suis dans le jeu console
-        enter=EnterValue
-        i=enter.EnterValue("Entrez le numéro de la carte que vous voulez jouer : ", 1, len(self.__cardsSet))
-        card_position=enter.EnterValue("Dénoncez vous le joueur précédement? (1: Oui, 2: Non) : ", 1, 2)
-        if card_position==1:
+        print(f"Le joueur {player.get_name()} avez choisi de dénoncer ")
+  
             #print les cartes et vérifier si la carte était celle demandé
-        else:
-            #jouer play_card()
-            pass
+        
+    
