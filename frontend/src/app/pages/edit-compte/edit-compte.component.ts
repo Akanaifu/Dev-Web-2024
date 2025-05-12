7ac26ed09d6ae84b357e2ede9196ad4710cab8ef
Nathan <153100527+Akanaifu@users.noreply.github.com>
2025-05-12 19:04:31 +0200

    feat: add EditCompteComponent and integrate route for account management

---
+++ frontend/src/app/pages/edit-compte/edit-compte.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-compte',
+  imports: [],
+  templateUrl: './edit-compte.component.html',
+  styleUrl: './edit-compte.component.css'
+})
+export class EditCompteComponent {
+
+}
