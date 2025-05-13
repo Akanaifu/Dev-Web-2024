4f95d436267e94ff518953324ccba5aec3041742
ben <abenpro@outlook.fr>
2025-05-13 14:41:40 +0200

    modif

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -4,7 +4,7 @@ console.log('🔍 Configuration DB:', {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306
+  port: process.env.DB_PORT 
 });
 
 const pool = mysql.createPool({
@@ -12,7 +12,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER || "dev",
   password: process.env.DB_PASSWORD || "casino",
   database: process.env.DB_NAME || "casino",
-  port: process.env.DB_PORT || "3306",
+  port: process.env.DB_PORT || "3310",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
