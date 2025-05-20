afc0a69f72067cbbe2e72955da0bb7e56276fa89
Nathan <153100527+Akanaifu@users.noreply.github.com>
2025-05-20 16:46:04 +0200

    fix: update game session entries and enhance Bets table structure with profit calculations

--- db/queries/data-casino.sql
+++ db/queries/data-casino.sql
@@ -8,12 +8,10 @@ INSERT INTO `User` (`user_id`, `username`, `name`, `firstname`, `birthdate`, `so
 
 -- Insérer des sessions de jeu dans la table Games_session
 INSERT INTO `Games_session` (`game_session_id`, `name`, `bet_min`, `bet_max`) VALUES
-('MA01', 'Slot Machine', '1', '100'), -- MA pour Machine à sous
 ('PO01', 'Poker', '5', '500'),       -- PO pour Poker
 ('RO01', 'Roulette', '2', '200'),    -- RO pour Roulette
 ('BJ01', 'Blackjack', '10', '1000'), -- BJ pour Blackjack
 ('BA01', 'Baccarat', '20', '2000'),  -- BA pour Baccarat
-('MA02', 'Slot Machine', '1', '150'), -- Deuxième session de Machine à sous
 ('PO02', 'Poker', '10', '1000'),      -- Deuxième session de Poker
 ('RO02', 'Roulette', '5', '300');     -- Deuxième session de Roulette
 
@@ -35,6 +33,13 @@ INSERT INTO `Bets` (`bet_id`,`user_id`, `game_session_id`, `amount`, `bet_status
 (7,2, 'PO02', 50.00, 'lose', NULL),    -- Deuxième session de Poker sans combinaison
 (8,3, 'RO02', 10.00, 'win', NULL);    -- Deuxième session de Roulette sans combinaison
 
+INSERT INTO `Bets` (`bet_id`,`user_id`, `game_session_id`, `amount`, `profit`, `bet_status`, `combinaison`) VALUES
+(2,2, 'PO01', 20.00, 0.00, 'lose', NULL),    -- Poker sans combinaison
+(3,3, 'RO01', 5.00, 15.00, 'win', NULL),     -- Roulette sans combinaison
+(4,2, 'BJ01', 100.00, 0.00, 'lose', NULL),   -- Blackjack sans combinaison
+(5,1, 'BA01', 200.00, 500.00, 'win', NULL),  -- Baccarat sans combinaison
+(7,2, 'PO02', 50.00, 0.00, 'lose', NULL),    -- Deuxième session de Poker sans combinaison
+(8,3, 'RO02', 10.00, 30.00, 'win', NULL);    -- Deuxième session de Roulette sans combinaison
 
 -- Insérer des statistiques dans la table Stats
 INSERT INTO `Stats` (`stat_id`,`user_id`, `num_games`, `num_wins`, `timestamp`) VALUES
