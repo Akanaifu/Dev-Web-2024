9af4a4277ffa20b26b09aff1bda8a7a96d2e8654
Nathan <153100527+Akanaifu@users.noreply.github.com>
2025-05-21 16:55:59 +0200

    refactor: remove unused route files for data, games, and transactions

--- db/queries/data-casino.sql
+++ db/queries/data-casino.sql
@@ -33,14 +33,6 @@ INSERT INTO `Bets` (`bet_id`,`user_id`, `game_session_id`, `amount`, `bet_status
 (7,2, 'PO02', 50.00, 'lose', NULL),    -- Deuxième session de Poker sans combinaison
 (8,3, 'RO02', 10.00, 'win', NULL);    -- Deuxième session de Roulette sans combinaison
 
-INSERT INTO `Bets` (`bet_id`,`user_id`, `game_session_id`, `amount`, `profit`, `bet_status`, `combinaison`) VALUES
-(2,2, 'PO01', 20.00, 0.00, 'lose', NULL),    -- Poker sans combinaison
-(3,3, 'RO01', 5.00, 15.00, 'win', NULL),     -- Roulette sans combinaison
-(4,2, 'BJ01', 100.00, 0.00, 'lose', NULL),   -- Blackjack sans combinaison
-(5,1, 'BA01', 200.00, 500.00, 'win', NULL),  -- Baccarat sans combinaison
-(7,2, 'PO02', 50.00, 0.00, 'lose', NULL),    -- Deuxième session de Poker sans combinaison
-(8,3, 'RO02', 10.00, 30.00, 'win', NULL);    -- Deuxième session de Roulette sans combinaison
-
 -- Insérer des statistiques dans la table Stats
 INSERT INTO `Stats` (`stat_id`,`user_id`, `num_games`, `num_wins`, `timestamp`) VALUES
 (1,1, 10, 5, '2025-04-20 10:00:00'),
