e6d9f751bb4c82dd238c3b301d9aed4537925bce
FrB-Toinou <115419687+FrB-Toinou@users.noreply.github.com>
2025-05-27 11:05:58 +0200

    Update commandes.bat
    update version du main

--- db/queries/commandes.bat
+++ db/queries/commandes.bat
@@ -26,4 +26,7 @@ mysql -u %MYSQL_USER% -p%MYSQL_PASSWORD% --port=3307 %DB_NAME% < %SQL_STRUCTURE%
 echo Importer les donnees depuis %SQL_DATA%...
 mysql -u %MYSQL_USER% -p%MYSQL_PASSWORD% --port=3307 %DB_NAME% < %SQL_DATA%
 
-echo Operation terminee.
+:: lancer le serveur
+echo Lancer le serveur MySQL...
+node ../../backend/server.js
+pause
