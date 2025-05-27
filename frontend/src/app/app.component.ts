98e164e73bf0f0f456b1aa56485c100b5fae4c45
Antoine Brazier <antoinefrb.brazier@gmail.com>
2025-05-27 09:45:54 +0200

    error : mauvaise gestion de git

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component, ViewChild } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavigationBarComponent } from './component/navigation-bar/navigation-bar.component'
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
+import { ChatComponent } from './component/chat-multy/chat-multy.component';
+import { LoginService } from './services/login/login.service';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports:[
+    RouterOutlet, 
+    NavigationBarComponent, 
+    MatButtonModule, 
+    MatTooltipModule, 
+    ChatComponent,
+
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+
+export class AppComponent {
+  @ViewChild('chatComponent') chatComponent!: ChatComponent;
+
+  constructor(public loginService: LoginService) {}
+
+  toggleChat(): void {
+    this.chatComponent.toggle();
+  }
+}
