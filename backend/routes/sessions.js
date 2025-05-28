621c8d8f35112b093ab2e0823aa4028da53723f5
Antoine Brazier <antoinefrb.brazier@gmail.com>
2025-05-28 10:47:11 +0200

    update : ligne manquant

--- backend/routes/sessions.js
+++ backend/routes/sessions.js
@@ -22,6 +22,7 @@ router.post('/login', async (req, res) => {
       ? 'SELECT user_id, username, email, password FROM User WHERE email = ?' 
       : 'SELECT user_id, username, email, password FROM User WHERE username = ?';
     
+    const [rows] = await db.query(query, [username]);
     
     if (rows.length === 0) {
       return res.status(401).json({ message: 'Identifiants incorrects' });
