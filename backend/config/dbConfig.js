de1673377884306fa3b7da3866511d972e1c602c
Antoine Brazier <antoinefrb.brazier@gmail.com>
2025-05-30 17:20:24 +0200

    update : version sans les ports

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -1,9 +1,11 @@
+const mysql = require('mysql2');
+
 const pool = mysql.createPool({
   host: "localhost", // Assurez-vous que c'est l'hôte correct
   user: "root", // Assurez-vous que c'est l'utilisateur correct
-  password: "c@sino", // Assurez-vous que c'est le mot de passe correct
+  password: "casino", // Assurez-vous que c'est le mot de passe correct
   database: "dev3", // Assurez-vous que c'est le nom de la base de données correct
-  port: 3307, // Assurez-vous que c'est le port correct
+  
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
