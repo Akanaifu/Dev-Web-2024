1b828740cbcde1b91c48de6d5251f5cda216ba76
Nathan <153100527+Akanaifu@users.noreply.github.com>
2025-05-30 19:57:54 +0200

    feat(machine-a-sous): integrate UserService for balance notifications and update constructor

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavigationBarComponent } from './component/navigation-bar/navigation-bar.component'
+import { NavigationBarComponent } from './component/navigation-bar/navigation-bar.component';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatButtonModule } from '@angular/material/button';
 import { ChatComponent } from './component/chat-multy/chat-multy.component';
@@ -9,19 +9,18 @@ import { LoginService } from './services/login/login.service';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports:[
-    RouterOutlet, 
-    NavigationBarComponent, 
-    MatButtonModule, 
-    MatTooltipModule, 
-    ChatComponent
-
+  imports: [
+    RouterOutlet,
+    NavigationBarComponent,
+    MatButtonModule,
+    MatTooltipModule,
+    ChatComponent,
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-
 export class AppComponent {
+  title = 'frontend';
   @ViewChild('chatComponent') chatComponent!: ChatComponent;
 
   constructor(public loginService: LoginService) {}
